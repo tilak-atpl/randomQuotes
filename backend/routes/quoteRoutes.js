b7563166af9ab0019b1063d8e57a550592a5843c
tilak-atpl <tilak.ram@autopeepal.com>
2024-08-29 15:43:50 +0530

    added delete functionaliyt

--- backend/routes/quoteRoutes.js
+++ backend/routes/quoteRoutes.js
@@ -1,29 +1,50 @@
-const express = require('express');
-const axios = require('axios');
-const Quote = require('../models/Quote');
+const express = require("express");
+const axios = require("axios");
+const Quote = require("../models/Quote");
 
 const router = express.Router();
 
- 
-
 // Save Favorite Quote
-router.post('/favourites', async (req, res) => {
+router.post("/add/favourites", async (req, res) => {
   try {
     const { content, author } = req.body;
     const quote = await Quote.create({ content, author });
     res.json(quote);
   } catch (error) {
-    res.status(500).send('Error saving favorite quote');
+    res.status(500).send("Error saving favorite quote");
   }
 });
 
 // Get Favorite Quotes
-router.get('/favourites', async (req, res) => {
+router.get("/get/favourites", async (req, res) => {
   try {
     const quotes = await Quote.findAll();
     res.json(quotes);
   } catch (error) {
-    res.status(500).send('Error fetching favorite quotes');
+    res.status(500).send("Error fetching favorite quotes");
+  }
+});
+
+router.delete("/delete/favourites", async (req, res) => {
+  try {
+    // Retrieve the quoteId from the query parameters
+    const quoteId = req.query.quoteId;
+
+    // Check if quoteId is provided
+    if (!quoteId) {
+      return res.status(400).json("quoteId is required.");
+    }
+
+    // Use the 'destroy' method with a condition to delete the specific record
+    const result = await Quote.destroy({ where: { id: quoteId } });
+
+    if (result) {
+      res.json("Deleted successfully.");
+    } else {
+      res.status(404).json("Quote not found.");
+    }
+  } catch (error) {
+    res.status(500).send("Error deleting favorite quote");
   }
 });
 
